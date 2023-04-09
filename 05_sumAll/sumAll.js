6b4767b302b8b9a0743052135da2095343dcf209
madjonga <madjonga123@gmail.com>
2023-04-09 16:15:28 +0300

    Do 05 task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR"
+    }
+    if (first < second) {
+        let nextNum = first
+        for (i = first; i <= second; i++) {
+            sum += nextNum
+            nextNum++
+        }
+        return sum
+    }
+    else if (first > second) {
+        let nextNum = second
+        for (i = second; i <= first; i++) {
+            sum += nextNum
+            nextNum++
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
