cc62163dac24d44a87ae27fea015e3f4a24e150c
madjonga <madjonga123@gmail.com>
2023-04-09 17:05:04 +0300

    Do 06 task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeap = true
+    if (year % 400 == 0) {
+        isLeap = true
+    }
+    else if (year % 100 == 0) {
+        isLeap = false
+    }
+    else if (year % 4 == 0) {
+        isLeap = true
+    }
+    else {
+        isLeap = false
+    }
+    return isLeap
 };
 
 // Do not edit below this line
