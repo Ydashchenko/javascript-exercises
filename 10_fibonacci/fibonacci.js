1d302f133d3b8bce192cfb8cdddf12c6babdb661
madjonga <madjonga123@gmail.com>
2023-04-15 19:17:40 +0300

    Do 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibArray = [1, 1]
+    if (index < 1) {
+        return 'OOPS'
+    } else if (index == 1) {
+        return fibArray[index]
+    } else {
+        for (i = 1; i < index - 1; i++) {
+            let newEl = fibArray[fibArray.length-1] + fibArray[fibArray.length-2]
+            fibArray.push(newEl)
+        }
+        console.log(fibArray)
+        return fibArray[index-1]
+    }
 };
 
 // Do not edit below this line
